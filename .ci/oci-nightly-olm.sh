b9655dcda907fb73616a44d0c9ace37417a69db7
Flavius Lacatusu <flacatus@redhat.com>
2020-12-02 09:54:50 +0100

    Test CI
    Signed-off-by: Flavius Lacatusu <flacatus@redhat.com>

---
+++ .ci/oci-nightly-olm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Copyright (c) 2012-2020 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+set -e
+
+# Detect the base directory where che-operator is cloned
+SCRIPT=$(readlink -f "$0")
+export SCRIPT
+
+OPERATOR_REPO=$(dirname "$(dirname "$SCRIPT")");
+export OPERATOR_REPO
+
+oc get pods -n che
