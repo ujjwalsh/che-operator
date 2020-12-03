abdd5fb16fdab1abe4563e0891a3e128a3a2c0fc
Flavius Lacatusu <flacatus@redhat.com>
2020-12-03 18:31:04 +0100

    Final script
    Signed-off-by: Flavius Lacatusu <flacatus@redhat.com>

--- .ci/oci-nightly-olm.sh
+++ .ci/oci-nightly-olm.sh
@@ -8,11 +8,24 @@
 # SPDX-License-Identifier: EPL-2.0
 #
 
+################################ !!!   IMPORTANT   !!! ################################
+########### THIS JOB USE openshift ci operators workflows to run  #####################
+##########  More info about how it is configured can be found here: https://docs.ci.openshift.org/docs/how-tos/testing-operator-sdk-operators #############
+#######################################################################################################################################################
+
 export XDG_CONFIG_HOME=/tmp/chectl/config
 export XDG_CACHE_HOME=/tmp/chectl/cache
 export XDG_DATA_HOME=/tmp/chectl/data
 
-set -ex
+# exit immediately when a command fails
+set -e
+# only exit with zero if all commands of the pipeline exit successfully
+set -o pipefail
+# error on unset variables
+set -u
+
+#Stop execution on any error
+trap "catchFinish" EXIT SIGINT
 
 # Detect the base directory where che-operator is cloned
 SCRIPT=$(readlink -f "$0")
@@ -21,7 +34,7 @@ export SCRIPT
 OPERATOR_REPO=$(dirname "$(dirname "$SCRIPT")");
 export OPERATOR_REPO
 
-# ENV used by Openshift CI
+# Artifacts dir where job will store all che events and logs
 ARTIFACTS_DIR="/tmp/artifacts"
 export ARTIFACTS_DIR
 
@@ -33,70 +46,74 @@ export PLATFORM
 NAMESPACE="eclipse-che"
 export NAMESPACE
 
-#
-export
-OPERATOR_IMAGE=${CI_CHE_OPERATOR_IMAGE:-"quay.io/eclipse/che-operator:nightly"}
+# CI_CHE_OPERATOR_IMAGE it is che operator image builded in openshift CI job workflow. More info about how works image dependencies in ci:https://github.com/openshift/ci-tools/blob/master/TEMPLATES.md#parameters-available-to-templates
+export OPERATOR_IMAGE=${CI_CHE_OPERATOR_IMAGE:-"quay.io/eclipse/che-operator:nightly"}
 
+# Get nightly CSV
 export CSV_FILE
 CSV_FILE="${OPERATOR_REPO}/deploy/olm-catalog/eclipse-che-preview-${PLATFORM}/manifests/che-operator.clusterserviceversion.yaml"
 
+# Import common functions utilities
 source "${OPERATOR_REPO}"/.ci/util/ci_common.sh
 
-function patchCheOperatorImage() {
-    echo "[INFO] Getting che operator pod name..."
-    operatorPod=$(oc get pods -o json -n ${NAMESPACE} | jq -r '.items[] | select(.metadata.name | test("che-operator-")).metadata.name')
-    oc patch pod ${operatorPod} -n ${NAMESPACE} --type='json' -p='[{"op": "replace", "path": "/spec/containers/0/image", "value":'${OPERATOR_IMAGE}'}]'
-    
-    # The following command retrieve the operator image
-    operatorImage=$(oc get pods -n ${NAMESPACE} -o json | jq -r '.items[] | select(.metadata.name | test("che-operator-")).spec.containers[].image')
-    echo "[INFO] CHE operator image is ${operatorImage}"
-}
+# catchFinish is executed after finish script.
+function catchFinish() {
+  result=$?
+  mkdir -p ${ARTIFACTS_DIR}/che-logs
 
-function waitCheServerDeploy() {
-  echo "[INFO] Waiting for Che server to be deployed"
-  set +e -x
-
-  i=0
-  while [[ $i -le 480 ]]
-  do
-    status=$(oc get checluster/eclipse-che -n "${NAMESPACE}" -o jsonpath={.status.cheClusterRunning})
-    oc get pods -n "${NAMESPACE}"
-    if [ "${status:-UNAVAILABLE}" == "Available" ]
-    then
-      break
-    fi
-    sleep 10
-    ((i++))
-  done
-
-  if [ $i -gt 480 ]
-  then
-    echo "[ERROR] Che server did't start after 8 minutes"
+  if [ "$result" != "0" ]; then
+    echo "[ERROR] Please check openshift ci artifacts"
+    chectl server:logs --chenamespace=${NAMESPACE} --directory=${ARTIFACTS_DIR}/che-logs
     exit 1
   fi
+
+  echo "[INFO] Job finished Successfully.Please check the artifacts in openshift ci artifacts"
+  chectl server:logs --chenamespace=${NAMESPACE} --directory=${ARTIFACTS_DIR}/che-logs
+
+  exit $result
 }
 
-# run function run the tests in ci of custom catalog source.
-function run() {
+# Utility to print objects created by Openshift CI automatically
+function printOlmCheObjects() {
+  echo -e "[INFO] Operator Group object created in namespace ${NAMESPACE}:"
+  oc get operatorgroup -n "${NAMESPACE}" -o yaml
+
+  echo -e "[INFO] Catalog Source object created in namespace ${NAMESPACE}:"
+  oc get catalogsource -n "${NAMESPACE}" -o yaml
+
+  echo -e "[INFO] Subscription object created in namespace ${NAMESPACE}"
+  oc get subscription -n "${NAMESPACE}" -o yaml
+}
+
+# Patch che operator image with image builded from source in Openshift CI job.
+function patchCheOperatorImage() {
+    echo "[INFO] Getting che operator pod name..."
+    OPERATOR_POD=$(oc get pods -o json -n ${NAMESPACE} | jq -r '.items[] | select(.metadata.name | test("che-operator-")).metadata.name')
+    oc patch pod ${OPERATOR_POD} -n ${NAMESPACE} --type='json' -p='[{"op": "replace", "path": "/spec/containers/0/image", "value":'${OPERATOR_IMAGE}'}]'
+    
+    # The following command retrieve the operator image
+    OPERATOR_POD_IMAGE=$(oc get pods -n ${NAMESPACE} -o json | jq -r '.items[] | select(.metadata.name | test("che-operator-")).spec.containers[].image')
+    echo "[INFO] CHE operator image is ${OPERATOR_POD_IMAGE}"
+}
+
+# Run che deployment after patch operator image.
+function deployEclipseChe() {
     export OAUTH="false"
 
-    oc project ${NAMESPACE}
+    # Deploy Eclipse Che applying CR
     applyCRCheCluster
     waitCheServerDeploy
 
-    # Create and start a workspace
+    # Create a workspace
     getCheAcessToken
-    chectl workspace:create --start --devfile=$OPERATOR_REPO/.ci/util/devfile-test.yaml
+    chectl workspace:create --start --chenamespace=${NAMESPACE} --devfile=$OPERATOR_REPO/.ci/util/devfile-test.yaml
 
+    # Start a workspace and wait until workspace it is alive
     getCheAcessToken
-    chectl workspace:list
+    chectl workspace:list --chenamespace=${NAMESPACE}
     waitWorkspaceStart
-
-    oc get pods -n $NAMESPACE
 }
 
+printOlmCheObjects
 patchCheOperatorImage
-run
-
-# grab che-operator namespace events after running olm nightly tests
-oc get events -n ${NAMESPACE} | tee ${ARTIFACTS_DIR}/che-operator-events.log
+deployEclipseChe
