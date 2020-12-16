5bc14b71785186b2d09b167634ee49eb05033f17
Flavius Lacatusu <flacatus@redhat.com>
2020-12-16 13:09:46 +0100

    fix
    Signed-off-by: Flavius Lacatusu <flacatus@redhat.com>

--- .ci/cico_updates_openshift.sh
+++ .ci/cico_updates_openshift.sh
@@ -22,10 +22,11 @@ OPERATORGROUP=$(
 apiVersion: operators.coreos.com/v1
 kind: OperatorGroup
 metadata:
-  name: che-ogroup
+  name: operatorgroup
   namespace: $OO_INSTALL_NAMESPACE
 spec:
-  targetNamespaces: [$OO_TARGET_NAMESPACES]
+  targetNamespaces:
+  - $OO_INSTALL_NAMESPACE
 EOF
 )
 
@@ -38,42 +39,68 @@ metadata:
   namespace: $OO_INSTALL_NAMESPACE
 spec:
   sourceType: grpc
-  image: "$CI_CATALOG_SOURCE_IMAGE"
+  image: quay.io/flavius_lacatusu/che-bundles:latest
 EOF
 )
 
 echo "CatalogSource name is \"$CATSRC\""
 echo "Creating Subscription"
 
+
+export platform="openshift"
+export OPERATOR_REPO=$(dirname $(dirname $(readlink -f "$0")));
+export channel="stable"
+export INSTALLATION_TYPE="catalog"
+
+packageName=eclipse-che-preview-${platform}
+platformPath=${OPERATOR_REPO}/olm/${packageName}
+packageFolderPath="${platformPath}/deploy/olm-catalog/${packageName}"
+packageFilePath="${packageFolderPath}/${packageName}.package.yaml"
+
+LATEST_CSV_NAME=$(yq -r ".channels[] | select(.name == \"${channel}\") | .currentCSV" "${packageFilePath}")
+lastPackageVersion=$(echo "${LATEST_CSV_NAME}" | sed -e "s/${packageName}.v//")
+PREVIOUS_CSV_NAME=$(sed -n 's|^ *replaces: *\([^ ]*\) *|\1|p' "${packageFolderPath}/${lastPackageVersion}/${packageName}.v${lastPackageVersion}.clusterserviceversion.yaml")
+PACKAGE_VERSION=$(echo "${PREVIOUS_CSV_NAME}" | sed -e "s/${packageName}.v//")
+INSTALLATION_TYPE="Marketplace"
+
+source "${OPERATOR_REPO}/olm/olm.sh" "${platform}" "${PACKAGE_VERSION}" "$OO_INSTALL_NAMESPACE" "${INSTALLATION_TYPE}"
+source "${OPERATOR_REPO}"/.github/bin/common.sh
+
 SUB=$(
     oc create -f - -o jsonpath='{.metadata.name}' <<EOF
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
 metadata:
-  generateName: oo-
+  name: $packageName
   namespace: $OO_INSTALL_NAMESPACE
 spec:
   name: eclipse-che-preview-openshift
-  channel: "stable"
+  channel: stable
+  installPlanApproval: Manual
   source: $CATSRC
   sourceNamespace: $OO_INSTALL_NAMESPACE
+  startingCSV: ${PREVIOUS_CSV_NAME}
 EOF
 )
+kubectl describe subscription/"$SUB" -n "${OO_INSTALL_NAMESPACE}"
 
-echo "Subscription naeeeme is \"$SUB\""
-echo "Waiting for ClusterServiceVersion to become ready..."
+kubectl wait subscription/"$SUB" -n "${OO_INSTALL_NAMESPACE}" --for=condition=InstallPlanPending --timeout=240s
 
-for _ in $(seq 1 30); do
-    oc describe subscription -n $OO_INSTALL_NAMESPACE
-    CSV=$(oc -n "$OO_INSTALL_NAMESPACE" get subscription "$SUB" -o jsonpath='{.status.installedCSV}' || true)
-    if [[ -n "$CSV" ]]; then
-        if [[ "$(oc -n "$OO_INSTALL_NAMESPACE" get csv "$CSV" -o jsonpath='{.status.phase}')" == "Succeeded" ]]; then
-            echo "ClusterServiceVersion \"$CSV\" ready"
-            exit 0
-        fi
-    fi
-    sleep 10
-done
+if [ $? -ne 0 ]
+then
+  echo Subscription failed to install the operator
+  exit 1
+fi
+
+kubectl describe subscription/"$SUB" -n "${OO_INSTALL_NAMESPACE}"
+
+installPackage
+echo -e "\u001b[32m Installation of the previous che-operator version: ${PREVIOUS_CSV_NAME} successfully completed \u001b[0m"
+applyCRCheCluster
+waitCheServerDeploy
+
+installPackage
+echo -e "\u001b[32m Installation of the latest che-operator version: ${LATEST_CSV_NAME} successfully completed \u001b[0m"
 
 sleep 2m
 
